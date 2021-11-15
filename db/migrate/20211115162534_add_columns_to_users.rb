fd384ea1f7d8b1384d623ceb86af5115da46e1e5
yasmine kordjazy <ykordjaz@gmail.com>
2021-11-15 18:06:42 +0100

    testing

---
+++ db/migrate/20211115162534_add_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :age, :string
+    add_column :users, :drivers_license, :string
+    add_column :users, :address, :string
+  end
+end
