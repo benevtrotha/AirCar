197fd8fef837b5de3a59cb946b40d9291794710b
Noemie Brancourt <noemie_brancourt@hotmail.com>
2021-11-16 17:57:04 +0100

    Added brand dropdown on new car form & some frontend on navbar

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,13 @@
 class Car < ApplicationRecord
-  BRAND = ['Ferrari', 'Porsche', 'Lamborghini', 'Aston Martin', 'Rolls Royce', 'Audi', 'Mercedes', 'Tesla', 'BMW']
+  belongs_to :user
+  has_many :bookings
+
+  BRAND = ['Ferrari', 'Porsche', 'Lamborghini', 'Aston Martin', 'Rolls Royce',
+    'Audi', 'Mercedes', 'Tesla', 'BMW', 'Lexus', 'Jaguar', 'Cadillac', 'Maserati',
+    'Bugatti', 'Volvo', 'Land Rover']
+
+  validates :brand, presence: true
+  validates :car_type, presence: true
+  validates :location, presence: true
+
 end
