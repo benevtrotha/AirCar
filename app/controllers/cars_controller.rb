e4d1116e506ecfcc1cc87087d0e4f444647a0ae2
yasmine kordjazy <ykordjaz@gmail.com>
2021-11-16 12:01:44 +0100

    did the routes

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,17 @@
+class CarsController < ApplicationController
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.save
+    redirect_to car_path(@car)
+  end
+
+  private
+
+    def car_params
+      params.require(:car).permit(:car_type, :price, :description, :location, :brand)
+    end
+end
