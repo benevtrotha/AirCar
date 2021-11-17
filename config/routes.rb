b5a9e475c7ade93d512d8a97fc1a4a6b0add8c8f
yasmine kordjazy <ykordjaz@gmail.com>
2021-11-17 13:13:33 +0100

    did the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resources :cars do
     resources :bookings, only: [:new, :create]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: 'dashboard#show', as: :dashboard
 end
